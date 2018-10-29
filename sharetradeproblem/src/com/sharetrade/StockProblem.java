027fc3c975ab396411bbbe87182d68213a163e4c
Tushar Seth <tusharseth1990@gmail.com>
2018-10-29 21:14:30 +0530

    first commit

---
+++ sharetradeproblem/src/com/sharetrade/StockProblem.java
@@ -0,0 +1,26 @@
+package com.sharetrade;
+
+public class StockProblem {
+	 int maxProfit(int price[], int arr_size)  
+	    { 
+	        int max_price = price[1] - price[0]; 
+	        int min_element = price[0]; 
+	        int i; 
+	        for (i = 1; i < arr_size; i++)  
+	        { 
+	            if (price[i] - min_element > max_price) 
+	            	max_price = price[i] - min_element; 
+	            if (price[i] < min_element) 
+	                min_element = price[i]; 
+	        } 
+	        return max_price; 
+	    } 
+	public static void main(String[] args) {
+		StockProblem stockproblem = new StockProblem(); 
+        int price[] = {4, 10, 5, 12, 1, 3, 11, 9, 1, 2}; 
+        int size = price.length; 
+        System.out.println("MaximumDifference is " +  
+        		stockproblem.maxProfit(price, size)); 
+	}
+
+}
